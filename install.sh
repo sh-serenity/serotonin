0ba8499075cf5990de7c0c0b1a585b4912b19997
root <root@leela>
2020-07-05 17:00:56 +0300

    7771

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ var db *sql.DB
 func dbConnect() (db *sql.DB) {
         dbDriver := \"mysql\"
      dbUser := \"$DBUSER\"
-      dbPass := \"$DBPASS\"
+      dbPass  \"$DBPASS\"
       dbHost := \"$DBHOST\"
         dbPort := \"3306\"
       dbName := \"$DB\"
@@ -31,7 +31,7 @@ func dbConnect() (db *sql.DB) {
         return db
 }
 
-" > /opt/serotonin//d/dbcon.go
+" > /opt/serotonin/d/dbcon.go
 
 
 
@@ -42,4 +42,4 @@ sleep 30
 docker exec  $DBHOST mysql -uroot -p$MYROOT -e "create user $DBUSER identified by '$DBPASS'"
 docker exec  $DBHOST mysql -uroot -p$MYROOT -e "create database $DB"
 docker exec  $DBHOST mysql -uroot -p$MYROOT -e "grant all privileges on $DB.* to $DBUSER"
-docker exec  $DBHOST mysql -u$DBUSER -p$DBPASS $DB  < db.sql
+docker exec  $DBHOST mysql -uroot -p$MYROOT 5$DB < db.sql
